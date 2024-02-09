e28667abdc63109c2389ef269edb44c3eb9662fd
amanraghuvanshi <iamanraghuvanshi@gmail.com>
2024-02-09 10:27:33 +0530

    V1.1.2

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/amanraghuvanshi/golang-blockchain
 
 go 1.20
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require github.com/gorilla/mux v1.8.1
--- models/models.go
+++ models/models.go
@@ -1,6 +1,11 @@
 package models
 
 type Block struct {
+	PrevHash  string
+	Position  int
+	Data      BookCheckout
+	TimeStamp string
+	Hash      string
 }
 
 type Book struct {
@@ -12,6 +17,10 @@ type Book struct {
 }
 
 type BookCheckout struct {
+	BookID       string `json:"book_id"`
+	User         string `json:"user"`
+	CheckoutDate string `json:"checkout_date"`
+	isGenesis    bool   `json:"is_genesis"`
 }
 
 type BlockChain struct {
